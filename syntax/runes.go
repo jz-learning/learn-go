aa2aa5863972dd8e6b0d2f99a612116d05b9573d
James Zhang <jameszhanga@gmail.com>
2022-03-10 17:32:07 -0600

    Different variable naming convension

---
+++ syntax/runes.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println([]byte("Hello"))
+	// [72 101 108 108 111]
+
+	// Shows that all characters are encoded in int32
+	// which is called a rune
+}
--- syntax/variables.go
+++ syntax/variables.go
@@ -8,15 +8,29 @@ var c, python, java bool
 // declaration + initialization, no need type
 var i, j = 5, 10
 
+// naming convention
+var time int
+var Time int // will be exported
+var _ int    // works as well
+
+// int initializes as 0
+
 func someMath(x int) int {
-	// short hand declaration, no need type
+	// short hand declaration, no need var or type
 	multiple := 10
 	return x * multiple
 }
 
 func main() {
 	var i int
-	var tall, short, name = true, false, "bob"
+	tall, short, name := true, false, "bob"
+
+	// group variables together for readability
+	var (
+		age       int
+		address   string
+		birthDate string
+	)
 
 	fmt.Println(name, "is tall:", tall, "and is short:", short)
 	fmt.Println(i, j, c, python, java)
